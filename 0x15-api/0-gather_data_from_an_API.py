973b1b82cdc8a58a5ed81aa125af7238c1ed8c1f
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-29 19:39:34 +0000

    Add 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+
+    user_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_info = requests.get(base_url+"users/"+user_id).json()
+    all_todos = requests.get(base_url+"todos",
+                             params={"userId": user_id}).json()
+    compl_todos = requests.get(base_url+"todos",
+                               params={"userId": user_id,
+                                       "completed": "true"}).json()
+    n_tasks = len(all_todos)
+    n_compl = len(compl_todos)
+
+    print(f"Employee {user_info['name']} is done with tasks"
+          f"({n_compl}/{n_tasks}):")
+    for task in compl_todos:
+        print("\t", task["title"])
+    # print(user_todos)
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+# API: Application programming interface
+Friends don't let friends code in bash
