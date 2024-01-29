7bc219b0ce8a57206a6b1fb8af22a795cb7c9161
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-29 20:30:37 +0000

    Add 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,11 +16,10 @@ if __name__ == "__main__":
     user_info = requests.get(base_url+"users/"+user_id).json()
     all_todos = requests.get(base_url+"todos",
                              params={"userId": user_id}).json()
-    print(type(user_info))
     csv_name = f"{user_id}.csv"
-    with open(csv_name, 'wb') as out_file:
-        csv_writer = csv.writer(out_file)
-        for row in user_info:
-            csv_writer.writerow(row.values())
-
-    # print(user_todos)
+    with open(csv_name, 'w') as out_file:
+        csv_writer = csv.writer(out_file, quoting=csv.QUOTE_ALL)
+        for todo in all_todos:
+            slctd = [todo.get(key, "N/A") for key in ['completed', 'title']]
+            line = [user_info.get("id"), user_info.get("username")] + slctd
+            csv_writer.writerow(line)
