96e35a0172dfc3277af601eb538044b7954a44e1
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-02-14 03:11:03 +0000

    Add 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ prints the titles of the first 10 hot posts listed for a given subreddit.
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """Function definition"""
     base_url = "https://www.reddit.com"
     end_point = "/subreddits/search"
@@ -16,30 +16,13 @@ def number_of_subscribers(subreddit):
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
                Safari/537.36'}
-    req = requests.get('{}/r/{}/about/.json'.format(base_url, subreddit),
+    req = requests.get('{}/r/{}/.json?\
+                       sort=top&limit=10'.format(base_url, subreddit),
                        headers=headers, allow_redirects=False)
 
-    if req.status_code == 200 and 'subscribers' in req.json()['data']:
-        return req.json()['data']['subscribers']
+    print('{}/r/{}/about/.json?sort=top&limit=10'.format(base_url, subreddit))
+    if req.status_code == 200:
+        for post in req.json()['data']['children'][0:10]:
+            print(post['data']['title'])
     else:
-        return 0
-
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """Function definition"""
-    base_url = "https://www.reddit.com"
-    end_point = "/subreddits/search"
-
-    headers = {'Accept': 'application/json',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
-               Safari/537.36'}
-    req = requests.get('{}/r/{}/about/.json'.format(base_url, subreddit),
-                       headers=headers, allow_redirects=False)
-
-    if req.status_code == 200 and 'subscribers' in req.json()['data']:
-        return req.json()['data']['subscribers']
-    else:
-        return 0
+        print(None)
