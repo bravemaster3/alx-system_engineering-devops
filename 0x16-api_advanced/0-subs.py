e6ed8abd4d7b23e011645280ad5b6c8a8cb3c353
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-02-14 02:41:42 +0000

    changed the url

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,9 +13,13 @@ def number_of_subscribers(subreddit):
     end_point = "/subreddits/search"
 
     headers = {'Accept': 'application/json',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
-               Safari/537.36'}
+               'User-Agent': ' '.join([
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+                   'AppleWebKit/537.36 (KHTML, like Gecko)',
+                   'Chrome/97.0.4692.71',
+                   'Safari/537.36',
+                   'Edg/97.0.1072.62'
+               ])}
     req = requests.get('{}/r/{}/about/.json'.format(base_url, subreddit),
                        headers=headers, allow_redirects=False)
     if req.status_code != 200:
