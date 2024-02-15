c5be0526667829a360218b111bc6420ef7a73848
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-02-15 18:46:14 +0000

    Add 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes extension problem in php file
 
-exec {'Fix extension error phpp int php':
+exec {'Fix-extension-error-phpp-in-php-settings':
     command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
     }
