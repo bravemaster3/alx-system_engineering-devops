b489e42b28d6a31edfd949a3cfe722f98b0bc485
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-02-23 01:43:04 +0000

    updated 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,8 @@
 # Increasing the limit
 
-exec { 'set_ulimit':
-  command     => 'echo ULIMIT="-n 10000" > /etc/default/nginx',
-  path        => '/bin:/usr/bin',
-  onlyif      => 'test -f /etc/default/nginx',
-  refreshonly => true,
+file { '/etc/default/nginx':
+  ensure     => present,
+  content    => 'ULIMIT="-n 10000"\n',
   notify      => Service['nginx'],
 }
 
